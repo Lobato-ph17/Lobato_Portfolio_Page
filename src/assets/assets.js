ca50eeb75dc962333907642968eda29fe5007f62
Pedro Lobato <lobato.ph17@gmail.com>
2026-09-04 12:32:44 -0300

    feat: cria a funcionalidade de dark/light mode

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -4,8 +4,8 @@ import arrow_icon from "./arrow-icon.svg"
 import close_icon from "./close_icon.svg"
 import menu_icon from './menu_icon.svg'
 import menu_icon_dark from './menu_icon_dark.svg'
-// import sun_icon from './sun_icon.svg'
-// import moon_icon from './moon_icon.svg'
+import sun_icon from './sun_icon.svg'
+import moon_icon from './moon_icon.svg'
 
 export const assets = {
   logo,
@@ -13,9 +13,9 @@ export const assets = {
   arrow_icon,
   close_icon,
   menu_icon,
-  menu_icon_dark
-  // sun_icon,
-  // moon_icon
+  menu_icon_dark,
+  sun_icon,
+  moon_icon
 };
 
 export default assets;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import {assets} from '../assets/assets'
+import ThemeToggleBtn from './ThemeToggleBtn';
 
 const Navbar = ({theme, setTheme}) => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -24,6 +25,9 @@ const Navbar = ({theme, setTheme}) => {
         </div>
 
         <div className='flex items-center gap-2 sm:gap-4 cursor-pointer'>
+
+          <ThemeToggleBtn theme={theme} setTheme={setTheme}/>
+
           <img src={theme === 'dark' ? assets.menu_icon_dark : assets.menu_icon} alt="" onClick={()=> setSidebarOpen(true)} className='w-8 sm:hidden' />
 
           <a href="#contact-us" className='text-sm max-sm:hidden flex items-center gap-2 bg-primary text-white px-6 py-2.5 rounded-full cursor-pointer hover:scale-105 transition-all'>
