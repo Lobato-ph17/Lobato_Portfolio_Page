306f64818554d3fdc51a4d31888c1b6e762a0b05
Pedro Lobato <lobato.ph17@gmail.com>
2026-09-04 12:13:43 -0300

    feat: cria o componente Navbar, estilizado com tema dark/light e responsivo para mobile

---
+++ src/assets/assets.js
@@ -0,0 +1,21 @@
+import logo from "./lobato-icon.svg";
+import logo_dark from "./dark_logo.svg";
+import arrow_icon from "./arrow-icon.svg"
+import close_icon from "./close_icon.svg"
+import menu_icon from './menu_icon.svg'
+import menu_icon_dark from './menu_icon_dark.svg'
+// import sun_icon from './sun_icon.svg'
+// import moon_icon from './moon_icon.svg'
+
+export const assets = {
+  logo,
+  logo_dark,
+  arrow_icon,
+  close_icon,
+  menu_icon,
+  menu_icon_dark
+  // sun_icon,
+  // moon_icon
+};
+
+export default assets;
