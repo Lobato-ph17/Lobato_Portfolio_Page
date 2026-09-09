4a1abfd2381130c5dc320db96f69689e13849f03
Pedro Lobato <lobato.ph17@gmail.com>
2026-09-08 22:31:52 -0300

    feat:cria componente de contatos

---
+++ src/components/Contact.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className='relative overflow-hidden max-w-lg m-2 sm:m-4 rounded-xl border border-gray-200 dark:border-gray-700 shadow-2xl shadow-gray-100 dark:shadow-white/10'>
+        
+        <div className='pointer-events-none blur-2xl rounded-full bg-gradient-to-r from-[#064E3B] to-[#10B981] absolute z-0 transition-opacity duration-500 mix-blend-lighten opactiy-70'>
+            <div>
+                
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Contact
